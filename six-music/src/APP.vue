65228e734cf5d943a140f3c7e9b04a4bc769513b
YangHangBattle <1776501907@qq.com>
2020-11-14 14:23:06 +0800

    测试

--- six-music/src/APP.vue
+++ six-music/src/APP.vue
@@ -1,13 +1,11 @@
 <template>
-  <div>
-  </div>
+  <div></div>
 </template>
 
 <script>
 export default {
-  name: '',
-}
+  name: "App",
+};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
