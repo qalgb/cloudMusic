492b1b5437273d02957d7c3997aef5171e5959cc
YangHangBattle <1776501907@qq.com>
2020-11-19 09:57:14 +0800

    组件

--- six-music/src/pages/Mv/index.vue
+++ six-music/src/pages/Mv/index.vue
@@ -1,32 +1,377 @@
 <template>
-<div class="bg">
-  <div class="main">
-    
-    <MvContent />
+  <div class="bg">
+    <div class="main">
+      <!-- 内容 -->
+      <div class="mv">
+        <!-- 筛选 -->
+        <div class="mod_tag">
+          <div class="tag_list">
+            <h3 class="tag_tit">区域</h3>
+            <a
+              href="javascript:;"
+              class="tag_item "
+              v-for="(item, index) in areamv"
+              :key="index"
+              :class="[ereaTag === index ? 'contentTag' : '']"
+              @click="areaTitle(index)"
+              >{{ item }}</a
+            >
+          </div>
+        </div>
+        <!-- 内容头部 -->
+        <div class="mv_top">
+          <h2 class="mv_top_left">全部MV</h2>
+          <div class="mv_top_right">
+            <a
+              href="javascript:;"
+              class="top_right"
+              v-for="(item, index) in newMv"
+              :key="index"
+              :class="[newMvData === index ? 'top_right_click' : '']"
+              @click="isnewMv(index)"
+              >{{ item }}</a
+            >
+          </div>
+        </div>
+        <!-- 内容详情 -->
+        <div class="mv_content">
+          <ul class="mv_content_ul">
+            <li class="content_ul_li" v-for="item in allMv" :key="item.id">
+              <div class="ul_li_box">
+                <a href="javascript:;" class="li_box_a">
+                  <img
+                    class="box_a_img"
+                    v-lazy="item.cover"
+                    @click="goMvVideo"
+                  />
+                  <i class="box_a_i"></i>
+                </a>
+                <h3 class="li_box_h3">
+                  <a href="javascript;:" class="box_h3_a">{{ item.name }}</a>
+                </h3>
+
+                <div class="li_box_div" title="Keano">
+                  <a href="javascript:;">
+                    {{ item.artistName }}
+                  </a>
+                </div>
+                <div class="li_box_info">
+                  <span class="box_info_span"
+                    ><i class="box_info_span_i"></i>{{ item.playCount }}</span
+                  >
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <!-- 分页 -->
+        <div class="paging">
+          <a
+            href="javascript:;"
+            class="paging_a"
+            data-index="1"
+            title="上一页"
+            hidefocus=""
+          >
+            <span>&lt;</span>
+          </a>
+          <a
+            href="javascript:;"
+            class="paging_a paging_a_click"
+            data-index="1"
+            hidefocus=""
+            >1</a
+          >
+          <a href="javascript:;" class="paging_a" data-index="1" hidefocus=""
+            >2</a
+          >
+          <a href="javascript:;" class="paging_a" data-index="3" hidefocus=""
+            >3</a
+          >
+          <a href="javascript:;" class="paging_a" data-index="4" hidefocus=""
+            >4</a
+          >
+          <strong class="more">...</strong>
+          <a href="javascript:;" class="paging_a" data-index="50" hidefocus=""
+            >50</a
+          >
+          <a
+            href="javascript:;"
+            class="paging_a"
+            data-index="3"
+            title="下一页"
+            hidefocus=""
+          >
+            <span>&gt;</span>
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
-</div>
-  
 </template>
 <script>
-import MvContent from './MvContent'
+import { mapState } from "vuex";
 export default {
-  name: "Mv",
-  
-  components: {
-    MvContent,
+  name: "Tag",
+  data() {
+    return {
+      area: "全部",
+      order: "最新",
+      newMvData: 0,
+      newMv: ["最新", "最热"],
+      areamv: ["全部", "内地", "港台", "欧美", "日本", "韩国"],
+      ereaTag: 0,
+    };
   },
-  
+  // 计算属性
+  computed: {
+    // 通过vuex的辅助函数获取数据
+    ...mapState({
+      // 获取全部的mv
+      allMv: (state) => state.mv.allMv,
+    }),
+  },
+
+  mounted() {
+    // 获取MV数据
+    this.getMvList(this.area, this.order);
+  },
+  methods: {
+    // 分发
+    getMvList(area, order) {
+      this.$store.dispatch("getAllMv", {
+        area,
+        order,
+      });
+    },
+    areaTitle(index) {
+      // 获取当前点击的地区
+      const area = this.areamv[index];
+      this.area = area;
+      // 给当前点击的元素样式
+      this.ereaTag = index;
+      // 获取到数据
+      const query = {
+        area,
+        order: this.order,
+      };
+      this.$router.replace({ path: "/mv", query });
+      this.getMvList(area, this.order);
+    },
+    // 点击最新，最热
+    isnewMv(index) {
+      // 获取到参数
+      const order = this.newMv[index];
+      this.newMvData = index;
+
+      const query = {
+        area: this.area,
+        order,
+      };
+
+      //获取mv数据
+      this.$router.replace({ path: "/mv", query });
+      this.getMvList(this.area, order);
+    },
+    // 跳转到MvVideo页面
+    goMvVideo() {
+      this.$router.replace({ path: "/mv/mvvideo" });
+    },
+  },
 };
 </script>
 <style scoped>
-.bg{
-background: url("./images/bg_detail.jpg") 50% 0 repeat-x;
+.bg {
+  background: url("./images/bg_detail.jpg") 50% 0 repeat-x;
 }
 .main {
   max-width: 1200px;
   margin: 0 auto;
   position: relative;
 }
+/* 内容 */
+.mv {
+  position: relative;
+  margin-bottom: 30px;
+}
+/* 筛选 */
+.tag_list {
+  position: relative;
+  height: auto;
+  zoom: 1;
+  clear: both;
+  overflow: hidden;
+  line-height: 26px;
+  padding-left: 65px;
+}
+.mod_tag {
+  position: relative;
+  padding: 52px 0 0;
+}
+.tag_tit {
+  position: absolute;
+  top: 0;
+  left: 0;
+  font-weight: 400;
+  width: 65px;
+}
 
-
+a:hover {
+  color: green;
+  text-decoration: none;
+}
+.tag_item {
+  float: left;
+  padding: 0 8px;
+  margin: 0 24px 15px 0;
+}
+.contentTag {
+  background-color: #31c27c;
+  color: #fff !important;
+}
+.mv_top {
+  overflow: hidden;
+  height: 60px;
+}
+.mv_top_left {
+  float: left;
+  font-size: 24px;
+  font-weight: 400;
+  line-height: 58px;
+}
+.mv_top_right {
+  position: absolute;
+  top: 7px;
+  right: 0;
+  height: 40px;
+  line-height: 40px;
+  border-radius: 2px;
+  font-size: 0;
+  margin-top: 95px;
+}
+.top_right {
+  position: relative;
+  display: inline-block;
+  padding: 0 16px;
+  height: 38px;
+  position: relative;
+  font-size: 14px;
+  border-width: 1px;
+  border-style: solid;
+  border-right: 1px solid #000;
+}
+.top_right_new {
+  border-right: none;
+}
+.top_right_click {
+  background-color: #31c27c;
+  color: #fff !important;
+  border-color: #31c27c;
+}
+.mv_content_ul {
+  margin-right: -20px;
+  zoom: 1;
+}
+.content_ul_li {
+  float: left;
+  width: 25%;
+  padding-bottom: 44px;
+  overflow: hidden;
+}
+.ul_li_box {
+  position: relative;
+  margin-right: 20px;
+}
+.li_box_a {
+  position: relative;
+  display: block;
+  overflow: hidden;
+  padding-top: 56.5476%;
+  margin-bottom: 15px;
+}
+.li_box_a:hover .box_a_i {
+  transform: scale(1.2);
+  opacity: 1;
+  transition: all 2s;
+}
+.box_a_img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  transition: all 2s;
+}
+.li_box_a:hover .box_a_img {
+  transform: scale(1.2);
+  transition: transform 2s;
+}
+.box_a_i {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  margin: -35px;
+  background: url("./images/2.png");
+  opacity: 0;
+  width: 70px;
+  height: 70px;
+  transition: all 2s;
+}
+.li_box_h3 {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  font-weight: 400;
+  zoom: 1;
+}
+.li_box_div a,
+.li_box_div,
+.li_box_info {
+  color: #999 !important;
+}
+.li_box_div {
+  height: 24px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+.li_box_div a {
+  font-size: 14px;
+}
+.li_box_info {
+  line-height: 18px;
+  height: 20px;
+  font-size: 14px;
+}
+.box_info_span {
+  margin-right: 10px;
+}
+.box_info_span_i {
+  display: inline-block;
+  width: 19px;
+  height: 12px;
+  margin-right: 5px;
+  vertical-align: -1px;
+  background: url("./images/icon_sprite.png") -180px -20px no-repeat;
+}
+.paging {
+  clear: both;
+  text-align: center;
+  margin-bottom: 60px;
+  font-size: 0;
+}
+.paging_a {
+  font-size: 16px;
+  font-weight: 400;
+  display: inline-block;
+  line-height: 50px;
+  margin: 0 5px;
+  padding: 0 21px;
+  box-sizing: border-box;
+  min-width: 24px;
+}
+.paging_a_click {
+  color: #fff !important;
+  background-color: #31c27c;
+}
 </style>
--- six-music/src/pages/MvVideo/index.vue
+++ six-music/src/pages/MvVideo/index.vue
@@ -1,19 +1,271 @@
 <template>
-  <div>
-    <Video />
-    <Comment />
+  <div class="mod_mv">
+    <!-- 视频区 -->
+    <div class="section_inner">
+      <div class="mv_player">
+        <div class="mvvideo">
+          <div class="video_player">
+            <!-- 视频状态栏 -->
+            <div class="video_player_tool">
+              <!-- 进度条 -->
+              <div class="video_player_tool_state">
+                <div class="video_player_tool_state_bg"></div>
+                <div
+                  class="video_player_tool_state_load "
+                  style="width: 33.7781%;"
+                ></div>
+                <div
+                  class="video_player_tool_state_progress"
+                  style="width: 33.7781%;"
+                >
+                  <div class="video_player_tool_state_progress_bg"></div>
+                  <a
+                    href="javascript:;"
+                    class="video_player_tool_state_btn "
+                    style="z-index:4;"
+                  >
+                  </a>
+                </div>
+              </div>
+              <div class="btn">
+                <!-- 操作 播放 play 暂停 pause -->
+                <div class="left_btn">
+                  <a href="javascript:;" title="暂停" class="video_player_icon"
+                    ><i class="iconfont icon-bofang"></i>
+
+                    <!-- 播放 用video_player_play这个id 暂停 用video_player_pause 这个id -->
+                  </a>
+
+                  <!-- 时长 -->
+                  <div class="video_player_time">
+                    <span class="js_qv_time_current">00:00</span>/
+                    <span class="js_qv_time_total">00:00</span>
+                  </div>
+                </div>
+
+                <!-- 操作 音量 选中 加current -->
+                <div class="right_btn">
+                  <a href="javascript:;" title="音量" class="video_player_btn">
+                    <i class="iconfont icon-shengyin"></i>
+                  </a>
+
+                  <!-- 操作 播放模式 点击切换 -->
+                  <a
+                    href="javascript:;"
+                    title="循环播放"
+                    class="video_player_btn"
+                  >
+                    <i class="iconfont icon-xunhuan"></i>
+                  </a>
+                  <!-- 操作 全屏 -->
+                  <a href="javascript:;" title="全屏" class="video_player_btn">
+                    <i class="iconfont icon-quanping"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="mv_info">
+        <h1 class="mv_title">
+          <span class="mv_name" title="Honey (舞蹈版)">Honey (舞蹈版)</span>
+
+          <span class="mv_line">-</span>
+
+          <span class="mv_singer ">张艺兴</span>
+        </h1>
+        <span class="mv_listen">播放量：68.2万</span>
+        <div class="mv_toolbar">
+          <a href="javascript:;" class="mv_toolbar_item ">举报</a>
+          <i class="mv_toolbar_line"></i>
+          <a class="mv_toolbar_item" href="javascript:;">评论</a>
+          <i class="mv_toolbar_line"></i>
+          <a class="mv_toolbar_item" href="javascript:;">分享</a>
+          <i class="mv_toolbar_line"></i>
+          <a class="mv_toolbar_item" href="javascript:;">收藏</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-import Comment from "../../components/Comment"
-import Video from "./Video"
 export default {
   name:"MvVideo",
-  components: {
-    Video,
-    Comment,
-  }
 }
 </script>
 <style scoped>
+
+.mod_mv {
+  padding-top: 80px;
+  width: 100%;
+  background-color: rgb(49, 194, 124);
+  z-index: 3;
+}
+.section_inner {
+  position: relative;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+.mv_player {
+  background-color: #fff;
+  font-size: 14px;
+  line-height: 1.5;
+}
+.mvvideo {
+  width: 1200px;
+  height: 674px;
+}
+/* 功能按钮 */
+.btn {
+  display: flex;
+  justify-content: space-between;
+}
+/* 左功能按钮 */
+.video_player {
+  height: 100%;
+  background-color: #000;
+  color: #fff;
+}
+.video_player_icon:hover i{
+  color: rgb(12, 241, 115);
+}
+/* 时长 */
+.video_player_time {
+  margin-top: 3px;
+  margin-left: 20px;
+  line-height: 30px;
+}
+.right_btn{
+  margin-right: 40px;
+  width: 130px;
+  display: flex;
+  justify-content: space-between;
+}
+/* 右边功能按钮 */
+/* 音量 */
+.video_player_btn:hover i {
+  color: rgb(12, 241, 115);
+}
+.video_player_btn {
+  fill: currentColor;
+}
+.video_player i {
+  color: #fff;
+  font-size: 20px;
+}
+/* 暂停 */
+.video_player_icon {
+  float: left;
+  margin: 12px 0 0 25px;
+  width: 30px;
+  height: 30px;
+  fill: currentColor;
+}
+/* 视频数据 */
+.mv_info {
+  height: 80px;
+  position: relative;
+}
+.mv_title {
+  margin-top: 20px;
+  font-weight: 400;
+  float: left;
+  margin-right: 10px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  max-width: 650px;
+}
+.mv_name,
+/* 歌手名 */
+.mv_singer,
+.mv_title {
+  color: #fff;
+  font-size: 22px;
+}
+/* mv名 */
+.mv_name {
+  float: left;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  max-width: 350px;
+}
+.mv_line {
+  margin: 0 8px;
+}
+/* 播放量 */
+.mv_listen {
+  display: inline-block;
+  margin-top: 30px;
+  font-size: 14px;
+  color: rgb(94, 91, 91);
+}
+
+/* 状态 */
+.video_player_tool {
+  position: absolute;
+  bottom: 80px;
+  left: 0;
+  width: 100%;
+  height: 52px;
+  z-index: 3;
+}
+/* 视频播放进度条 */
+.video_player_tool_state_bg {
+  display: block;
+  height: 100%;
+  background: rgba(255, 255, 255, 0.2);
+}
+.video_player_tool_state {
+  position: relative;
+  width: 100%;
+  height: 3px;
+  cursor: pointer;
+}
+/* 举报，评论，分享，收藏 */
+.mv_toolbar {
+  position: absolute;
+  top: 0;
+  right: 0;
+  font-size: 0;
+  line-height: 78px;
+  height: 78px;
+  white-space: nowrap;
+}
+.mv_toolbar_item {
+  display: inline-block;
+  font-size: 20px;
+  vertical-align: middle;
+}
+.mv_toolbar_item:hover {
+  color: #fff !important;
+}
+.mv_toolbar_line {
+  display: inline-block;
+  width: 20px;
+  height: 11px;
+  margin-right: 20px;
+  border-right: 1px dotted #5b5b5c;
+  vertical-align: middle;
+}
+
+/* 时长 音量 全屏 循环*/
+.video_player_btn,
+.video_player_time {
+  position: relative;
+  float: left;
+  top: 9px;
+  font-size: 14px;
+}
+/* 音量 全屏 循环 */
+.video_player_btn:after {
+  content: "";
+  position: absolute;
+  left: -10px;
+  top: -5px;
+  bottom: -10px;
+  right: -10px;
+}
 </style>
